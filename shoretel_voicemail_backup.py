c2b664d090601de02606a08fcb0fd45fd4c9eee1
Aaron M <thisaaronmdev@gmail.com>
2017-10-27 22:31:15 -0700

    MANY revisions (bad message, I know)

--- shoretel_voicemail_backup.py
+++ shoretel_voicemail_backup.py
@@ -8,156 +8,152 @@ import shutil
 
 
 # -------------------------------- VARIABLES --------------------------------- #
-mailbox = "/Volumes/d$/Shoreline Data/Vms/SHORETEL"
-messages = "/Volumes/d$/Shoreline Data/Vms/Message"
+mailbox = "/Volumes/z$/Shoreline Data/Vms/SHORETEL"
+messages = "/Volumes/z$/Shoreline Data/Vms/Message"
 
 
 # -------------------------------- FUNCTIONS --------------------------------- #
 def check_path(p):
-    """
-    Check if required HQ/DVS directory is accessible.
-    """
-    if os.path.isdir(p):
-    	# print("\nINFO: {0} is accessible.\n".format(p))
-    	pass
-    elif not os.path.isdir(p):
-    	print("\nERROR: {0} is inaccessible.\n".format(p))
-    	sys.exit(10)
-    else:
-    	print("\nERROR: An unknown error has occurred.\n")
-    	# raise
-    	sys.exit(20)
+	"""
+	Check if required HQ/DVS directory is accessible.
+	"""
+	if os.path.isdir(p):
+		# print("\nINFO: {0} is accessible.\n".format(p))
+		pass
+	elif not os.path.isdir(p):
+		print("\nERROR: {0} is inaccessible.\n".format(p))
+		sys.exit(10)
+	else:
+		print("\nERROR: An unknown error has occurred.\n")
+		# raise
+		sys.exit(20)
 
 
 def check_argument():
-    """
-    Check for extension argument
-    """
-    try:
-    	ext = int(sys.argv[1])
-    	return ext
-    except IndexError as err_idx:
-    	print("\nERROR: Extension argument not provided.\n")
-    	sys.exit(30)
-    except ValueError as err_val:
-    	print("\nERROR: Extension argument is non-integer.\n")
-    	sys.exit(40)
-    except:
-    	print("\nERROR: An unknown error has occurred.\n")
-    	# raise
-    	sys.exit(50)
+	"""
+	Check for extension argument
+	"""
+	try:
+		ext = int(sys.argv[1])
+		return ext
+	except IndexError as err_idx:
+		print("\nERROR: Extension argument not provided.\n")
+		sys.exit(30)
+	except ValueError as err_val:
+		print("\nERROR: Extension argument is non-integer.\n")
+		sys.exit(40)
+	except:
+		print("\nERROR: An unknown error has occurred.\n")
+		# raise
+		sys.exit(50)
 
 
 def check_extension(p, e):
-    """
-    Checks for the mailbox of the provided extension.
-    """
-    if os.path.isdir(os.path.join(p, str(e))):
-    	print("\nINFO: Mailbox for {0} found.".format(str(e)))
-    else:
-    	print("\nERROR: There is no mailbox associated with that extension.\n")
-    	sys.exit(60)
+	"""
+	Checks for the mailbox of the provided extension.
+	"""
+	if os.path.isdir(os.path.join(p, str(e))):
+		print("\nINFO: Mailbox for {0} found.".format(str(e)))
+	else:
+		print("\nERROR: There is no mailbox associated with that extension.\n")
+		sys.exit(60)
 
 
 def check_message_ids(m_ids, e):
-    """
-    Check if there are any Message IDs (for the voicemails) for the provided extension.
-    If not, exit.
-    If so, create new directory on user's Desktop.
-    """
-    if len(m_ids) == 0:
-    	print("\nERROR: No voicemails for {0} found.\n".format(e))
-    	sys.exit(70)
-    else:
-    	try:
-    		## swap datenow comments to test FileExistsError exception
-    		# datenow = datetime.now().strftime('%Y%m%d')
-    		datenow = datetime.now().strftime('%Y%m%d_%H%M%S')
+	"""
+	Check if there are any Message IDs (for the voicemails) for the provided extension.
+	If not, exit.
+	If so, create new directory on user's Desktop.
+	"""
+	if len(m_ids) == 0:
+		print("\nERROR: No voicemails for {0} found.\n".format(e))
+		sys.exit(70)
+	else:
+		try:
+			## swap datenow comments to test FileExistsError exception
+			# datenow = datetime.now().strftime('%Y%m%d')
+			datenow = datetime.now().strftime('%Y%m%d_%H%M%S')
 
-    		homedir = os.path.expanduser('~')
-    		newdirname = "{0}_{1}".format(datenow, str(e))
-    		newdirpath = os.path.join(homedir, 'Desktop', newdirname)
+			homedir = os.path.expanduser('~')
+			newdirname = "{0}_{1}".format(datenow, str(e))
+			newdirpath = os.path.join(homedir, 'Desktop', newdirname)
 
-    		print("INFO: Attempting to create {0}.".format(newdirpath))
-    		os.mkdir(newdirpath)
-    		if os.path.isdir(newdirpath):
-    			print("INFO: Successfully created {0}.\n".format(newdirpath))
-    			return newdirpath
-    		else:
-    			print("\nERROR: Failed to create {0}.\n".format(newdirpath))
-    			sys.exit(80)
-    	except FileExistsError as err_fee:
-    		print("\nERROR: Unable to create {0}. It already exists.\n".format(newdirpath))
-    		sys.exit(90)
-    	except:
-    		print("\nERROR: An unknown error has occurred.\n")
-    		# raise
-    		sys.exit(100)
+			print("INFO: Attempting to create {0}.".format(newdirpath))
+			os.mkdir(newdirpath)
+			print("INFO: Successfully created {0}.".format(newdirpath))
+			return newdirpath
+		except FileExistsError as err_fee:
+			print("\nERROR: Unable to create {0}. It already exists.\n".format(newdirpath))
+			sys.exit(80)
+		except:
+			print("\nERROR: An unknown error has occurred.\n")
+			# raise
+			sys.exit(90)
 
 
 def backup_voicemails(message_ids):
-    """
-    Copies the WAVs from HQ/DVS to the new directory on the user's Desktop.
-    Renames the WAVs as the time the voicemail was received, and the caller ID.
-    	e.g. "2017-1026_130203_+18883222822.wav"
-    """
-    new_dir = check_message_ids(message_ids, extension)
-    for msg_id in message_ids:
-    	if os.path.isfile("{0}.wav".format(os.path.join(messages, msg_id))):
-    		print("\nINFO: {0}.wav exists and will be copied to {1}.".format(msg_id, new_dir))
-    		shutil.copy2("{0}.wav".format(os.path.join(messages, msg_id)), "{0}".format(new_dir))
-    		if os.path.isfile("{0}.wav".format(os.path.join(new_dir, msg_id))):
-    			print("INFO: Successfully copied {0}.wav to {1}.".format(msg_id, new_dir))
+	"""
+	Copies the WAVs from HQ/DVS to the new directory on the user's Desktop.
+	Renames the WAVs as the time the voicemail was received, and the caller ID.
+		e.g. "2017-1026_130203_+18883222822.wav"
+	"""
+	new_dir = check_message_ids(message_ids, extension)
+	for msg_id in message_ids:
+		if os.path.isfile("{0}.wav".format(os.path.join(messages, msg_id))):
+			print("\nINFO: {0}.wav exists and will be copied to {1}.".format(msg_id, new_dir))
+			shutil.copy2("{0}.wav".format(os.path.join(messages, msg_id)), "{0}".format(new_dir))
+			if os.path.isfile("{0}.wav".format(os.path.join(new_dir, msg_id))):
+				print("INFO: Successfully copied {0}.wav to {1}.".format(msg_id, new_dir))
 
-    			uxts = os.path.getmtime("{0}.wav".format(os.path.join(new_dir, msg_id)))
-    			ts = datetime.fromtimestamp(uxts).strftime('%Y-%m%d_%H%M%S')
-    			cid = pstrings("{0}.msg".format(os.path.join(messages, msg_id)), 1)
-    			new_wav_name = "{0}_{1}".format(ts, cid[3])
+				uxts = os.path.getmtime("{0}.wav".format(os.path.join(new_dir, msg_id)))
+				ts = datetime.fromtimestamp(uxts).strftime('%Y-%m%d_%H%M%S')
+				cid = pstrings("{0}.msg".format(os.path.join(messages, msg_id)), 1)
+				new_wav_name = "{0}_{1}".format(ts, cid[3])
 
-    			old_name = os.path.join(new_dir, msg_id)
-    			new_name = os.path.join(new_dir, new_wav_name)
+				old_name = os.path.join(new_dir, msg_id)
+				new_name = os.path.join(new_dir, new_wav_name)
 
-    			print("INFO: Renaming {0}.wav to {1}.wav".format(old_name, new_name))
-    			os.rename("{0}.wav".format(old_name), "{0}.wav".format(new_name))
-    			if os.path.isfile("{0}.wav".format(new_name)):
-    				print("INFO: Successfully renamed {0}.wav to {1}.wav\n".format(old_name, new_name))
-    			else:
-    				print("\nWARNING: Failed to rename {0}.wav to {1}.wav\n".format(old_name, new_name))
-    		else:
-    			print("\nWARNING: Failed to copy {0}.wav to {1}.\n".format(msg_id, new_dir))
-    	else:
-    		print("WARNING: {0}.wav does not exist.".format(msg_id))
+				print("INFO: Renaming {0}.wav to {1}.wav".format(old_name, new_name))
+				os.rename("{0}.wav".format(old_name), "{0}.wav".format(new_name))
+				if os.path.isfile("{0}.wav".format(new_name)):
+					print("INFO: Successfully renamed {0}.wav to {1}.wav".format(old_name, new_name))
+				else:
+					print("\nWARNING: Failed to rename {0}.wav to {1}.wav\n".format(old_name, new_name))
+			else:
+				print("\nWARNING: Failed to copy {0}.wav to {1}.\n".format(msg_id, new_dir))
+		else:
+			print("WARNING: {0}.wav does not exist.".format(msg_id))
 
 
 def pstrings(filename, min=9):
-    """
-    Python version of UNIX "strings" utility.
-    Adapted from: https://stackoverflow.com/a/17197027/5473041
-    """
-    if sys.version_info[0] < 3:
-    	f = open(filename, "rb")
-    else:
-    	f = open(filename, "r", errors="ignore")
+	"""
+	Python version of UNIX "strings" utility.
+	Adapted from: https://stackoverflow.com/a/17197027/5473041
+	"""
+	if sys.version_info[0] < 3:
+		f = open(filename, "rb")
+	else:
+		f = open(filename, "r", errors="ignore")
 
-    l = []
-    pstring = ""
-    for i in f.read():
-    	if i in string.printable:
-    		pstring += i
-    		continue
-    	if min == 9:			## use for getting Message IDs from Mailbox.dat
-    		if len(pstring) == min:
-    			l.append(pstring)
-    	else:			   		## use for getting Caller ID from msg_id.msg
-    		if len(pstring) > min:
-    			l.append(pstring)
-    	pstring = ""
+	l = []
+	pstring = ""
+	for i in f.read():
+		if i in string.printable:
+			pstring += i
+			continue
+		if min == 9:			## use for getting Message IDs from Mailbox.dat
+			if len(pstring) == min:
+				l.append(pstring)
+		else:			   		## use for getting Caller ID from msg_id.msg
+			if len(pstring) > min:
+				l.append(pstring)
+		pstring = ""
 
-    if len(pstring) > 0:
-    	l.append(pstring)
+	if len(pstring) > 0:
+		l.append(pstring)
 
-    f.close()
-    return l
+	f.close()
+	return l
 
 
 # --------------------------------- RUN IT! ---------------------------------- #
@@ -168,4 +164,4 @@ check_extension(mailbox, extension)
 message_ids = pstrings(os.path.join(mailbox, str(extension), "Mailbox.dat"))
 backup_voicemails(message_ids)
 
-print("Successfully completed voicemail backup of {0}.\n".format(extension))
+print("\nSuccessfully completed voicemail backup of {0}.\n".format(extension))
--- shoretel_voicemail_backup.sh
+++ shoretel_voicemail_backup.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # -------------------------------- VARIABLES --------------------------------- #
-st_mailbox="/Volumes/z$/Shoreline Data/Vms/SHORETEL"
-st_messages="/Volumes/z$/Shoreline Data/Vms/Message"
-st_integer='^[0-9]+$'
+mailbox="/Volumes/z$/Shoreline Data/Vms/SHORETEL"
+messages="/Volumes/z$/Shoreline Data/Vms/Message"
+integer='^[0-9]+$'
 
 
 # -------------------------------- FUNCTIONS --------------------------------- #
-function CheckMount () {
-  if [[ ! -d $st_mailbox ]]; then
+function check_path () {
+  if [[ ! -d $mailbox ]]; then
     echo
-    echo "$st_mailbox not found."
+    echo "$mailbox not found."
     echo
     exit 10
-  elif [[ ! -d $st_messages ]]; then
+  elif [[ ! -d $messages ]]; then
     echo
-    echo "$st_mailbox not found."
+    echo "$mailbox not found."
     echo
     exit 20
   fi
 }
 
 
-function CheckArgument () {
-  if [[ -z $1 ]] || ! [[ $1 =~ $st_integer ]]; then
-    while [[ -z $st_extension ]] || ! [[ $st_extension =~ $st_integer ]];
+function check_argument () {
+  if [[ -z $1 ]] || ! [[ $1 =~ $integer ]]; then
+    while [[ -z $extension ]] || ! [[ $extension =~ $integer ]];
     do
       echo
       echo '####################################################################'
@@ -34,22 +34,21 @@ function CheckArgument () {
       echo "    e.g.) ./shoretel_voicemail_backup.sh 1234"
       echo '####################################################################'
       echo
-      read -p "Enter the extension you wish to export voicemails for: " st_extension
+      read -p "Enter the extension you wish to export voicemails for: " extension
     done
   else
-    st_extension=$1
+    extension=$1
   fi
 }
 
 
-function CheckExtension () {
-  if [[ -e "$st_mailbox"/$st_extension ]]; then
-    folder=$HOME/Desktop/$(date +%Y-%m%d_%H%M%S_$st_extension)
+function check_extension () {
+  if [[ -e "$mailbox"/$extension ]]; then
+    folder=$HOME/Desktop/$(date +%Y-%m%d_%H%M%S_$extension)
     echo
     echo "Creating $folder"
     mkdir $folder
     echo "Directory created."
-    echo
   else
     echo
     echo "There is no mailbox associated with that extension."
@@ -59,33 +58,35 @@ function CheckExtension () {
 }
 
 
-function GetVoicemails () {
-  for i in $(strings "$st_mailbox"/$st_extension/Mailbox.dat | \
+function backup_voicemails () {
+  for i in $(strings "$mailbox"/$extension/Mailbox.dat | \
     awk 'length($1) == 9 {print $1}');
   do
-    if [[ -e "$st_messages"/$i.wav ]]; then
+    if [[ -e "$messages"/$i.wav ]]; then
       echo
       echo "$i.wav exists and will be copied."
-      cp -p "$st_messages"/$i.wav $folder/$i.wav
+      cp -p "$messages"/$i.wav $folder/$i.wav
       echo "Copying $i.wav complete."
       echo "Renaming $i.wav to $(stat -f %Sm -t %Y-%m%d_%H%M%S \
-        $folder/$i.wav)_$(strings "$st_messages"/$i.msg | \
+        $folder/$i.wav)_$(strings "$messages"/$i.msg | \
         awk 'FNR==4{print $0}').wav."
       mv $folder/$i.wav $folder/$(stat -f %Sm -t %Y-%m%d_%H%M%S \
-        $folder/$i.wav)_$(strings "$st_messages"/$i.msg | \
+        $folder/$i.wav)_$(strings "$messages"/$i.msg | \
         awk 'FNR==4{print $0}').wav
       echo "Renaming complete."
-      echo
     else
       echo
       echo "Skipping $i, because $i.wav does not exist."
-      echo
     fi
   done
 }
 
 # --------------------------------- RUN IT! ---------------------------------- #
-CheckMount
-CheckArgument $1
-CheckExtension
-GetVoicemails
+check_path
+check_argument $1
+check_extension
+backup_voicemails
+
+echo
+echo "Successfully completed voicemail backup of $1"
+echo
